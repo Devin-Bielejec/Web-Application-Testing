cccf94d96cea39a94864e84e60e1e0727563e2a7
Devin-Bielejec <devin.bielejec@gmail.com>
2019-09-01 13:56:32 -0600

    Started logic on app

--- baseball/src/App.js
+++ baseball/src/App.js
@@ -5,19 +5,22 @@ import DisplayComponent from "./components/DisplayComponent";
 import DashboardComponent from "./components/DashboardComponent";
 
 function App() {
-  const useInput = (initialValue) => {
+  const useInput = (key, initialValue) => {
     const [value, setValue] = useState(initialValue);
     const handleChanges = () => {
+        (key === "ball" && (value+1) === 4) ? setValue(0) : setValue(value+1);
+        (key === "strike" && (value+1) === 3) ? setValue(0) : setValue(value+1);
+        
         console.log(value);
         setValue(value + 1);
     };
     return [value, setValue, handleChanges];
     }
 
-    const [ball, setBall, ballChanges] = useInput(0);
-    const [strike, setStrike, strikeChanges] = useInput(0);
-    const [foul, setFoul, foulChanges] = useInput(0);
-    const [hit, setHit, hitChanges] = useInput(0);
+    const [ball, setBall, ballChanges] = useInput("ball", 0);
+    const [strike, setStrike, strikeChanges] = useInput("strike", 0);
+    const [foul, setFoul, foulChanges] = useInput("foul",0);
+    const [hit, setHit, hitChanges] = useInput("hit", 0);
 
   return (
     <div className="App">
