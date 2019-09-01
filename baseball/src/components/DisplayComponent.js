c8fa3cdd93e260e2797e0588eabd7d6b2ad73b95
Devin-Bielejec <devin.bielejec@gmail.com>
2019-09-01 13:34:14 -0600

    Barebones display component

--- baseball/src/components/DisplayComponent.js
+++ baseball/src/components/DisplayComponent.js
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 const DisplayComponent = () => {
-    
+    const useInput = (key, initialValue) => {
+        const [value, setValue] = useLocalStorage(key, initialValue);
+        const handleChanges = updatedValue => {
+            setValue(updatedValue);
+        };
+    return [value, setValue, handleChanges];
+    }
+
+    const [ball, setBall, ballChanges] = useInput("ball", 0);
+    const [strike, setStrike, strikeChanges] = useInput("strike", 0);
+    const [foul, setFoul, foulChanges] = useInput("foul", 0);
+    const [hit, setHit, hitChanges] = useInput("hit", 0);
+
+    return(
+        <div>
+            <p class="ballCount">Ball Count: {ball}</p>
+            <p class="strikeCount">Strike Count: {strike}</p>
+            <p class="foulCount">Foul Count: {foul}</p>
+            <p class="hitCount">Hit Count: {hit}</p>
+        </div>
+    )
+
+
 }
