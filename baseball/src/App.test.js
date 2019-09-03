7fea147e210e4bc31f56c964209c717a96e2322e
devin-bielejec <devin.bieleje@gmail.com>
2019-09-03 11:15:02 -0400

    Organized tests better

--- baseball/src/App.test.js
+++ baseball/src/App.test.js
@@ -7,9 +7,11 @@ afterEach(rtl.cleanup);
 
 it('renders without crashing', () => {
   const simulatedDOM = rtl.render(<App />);
+})
 
-  //Check if buttons exist
-  const divButtons = document.querySelector("div.buttons");
+it("All Buttons exists", () => {
+  const simulatedDOM = rtl.render(<App />);
+
   const strikeButton = document.querySelector("div.buttons button.strike");
   expect(strikeButton).toBeInTheDocument();
 
@@ -21,9 +23,11 @@ it('renders without crashing', () => {
 
   const hitButton = document.querySelector("div.buttons button.hit");
   expect(hitButton).toBeInTheDocument();
+})
 
-  //Check if count is being displayed, 
-  const divCounts = document.querySelector("div.counts");
+it("Count is being displayed on all buttons", () => {
+  const simulatedDOM = rtl.render(<App />);
+
   const strikeCount = document.querySelector("div.counts p.strike");
   expect(strikeCount).toBeInTheDocument();
 
@@ -35,21 +39,40 @@ it('renders without crashing', () => {
 
   const hitCount = document.querySelector("div.counts p.hit");
   expect(hitCount).toBeInTheDocument();
+})
 
-  //Ensure that clicking strike, increases strike count by 1 until 3 then resets everything to 0
-  rtl.fireEvent.click(strikeButton, {button: 0});
-  expect(strikeCount).toHaveTextContent(1);
-  rtl.fireEvent.click(strikeButton, {button: 0});
-  expect(strikeCount).toHaveTextContent(2);
-  rtl.fireEvent.click(strikeButton, {button: 0});
-  const isReset = () => {
+const isReset = () => {
+    const hitCount = document.querySelector("div.counts p.hit");
+    const ballCount = document.querySelector("div.counts p.ball");
+    const foulCount = document.querySelector("div.counts p.foul");
+    const strikeCount = document.querySelector("div.counts p.strike");
+
     expect(strikeCount).toHaveTextContent(0);
     expect(ballCount).toHaveTextContent(0);
     expect(hitCount).toHaveTextContent(0);
     expect(foulCount).toHaveTextContent(0);
   }
 
-  //Ensure that clicking ball, increase ball count by 1 until 4 then resets everything to 0
+
+it("Strike increases count by 1 until 3 then resets everything to 0", () => {
+  const simulatedDOM = rtl.render(<App />);
+
+  const strikeButton = document.querySelector("div.buttons button.strike");
+  const strikeCount = document.querySelector("div.counts p.strike");
+  rtl.fireEvent.click(strikeButton, {button: 0});
+  expect(strikeCount).toHaveTextContent(1);
+  rtl.fireEvent.click(strikeButton, {button: 0});
+  expect(strikeCount).toHaveTextContent(2);
+  rtl.fireEvent.click(strikeButton, {button: 0});
+  
+  isReset();
+})
+  
+it("Clicking Ball Button increase count by 1 until 4", () => {
+  const simulatedDOM = rtl.render(<App />);
+
+  const ballButton = document.querySelector("div.buttons button.ball");
+  const ballCount = document.querySelector("div.counts p.ball");
   rtl.fireEvent.click(ballButton, {button: 0});
   expect(ballCount).toHaveTextContent(1);
   rtl.fireEvent.click(ballButton, {button: 0});
@@ -58,14 +81,25 @@ it('renders without crashing', () => {
   expect(ballCount).toHaveTextContent(3);
   rtl.fireEvent.click(ballButton, {button: 0});
   expect(ballCount).toHaveTextContent(0);
-  isReset();
 
-  //Ensure that clicking hit, resets everything to 0
+  isReset();
+})
+  
+it("Clicking hit button resets everything to 0", () => {
+  const simulatedDOM = rtl.render(<App />);
   rtl.fireEvent.click(hitButton);
   isReset();
+})
 
-  
-  console.log(simulatedDOM.debug());
+it("Clicking Foul Button increases by 1 everytime until at least 100", () => {
+  const simulatedDOM = rtl.render(<App />);
 
-});
+  const foulButton = document.querySelector("div.buttons button.foul");
+  const foulCount = document.querySelector("div.counts p.foul");
+  for (let i = 1; i < 100; i++){
+    rtl.fireEvent.click(foulButton);
+    expect(foulCount).toHaveTextContent(i);
+  }
+})
+
 
